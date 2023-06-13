98b9a3f47d6b3705ce8e167ace697b050d71ea52
Enrique <enriquedlc2d@gmail.com>
2023-06-14 00:12:21 +0100

    :heavy_check_mark: test admitting_delimiter passed.

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,13 +1,25 @@
 public class StringCalculator {
     public int add(String numbers) {
-        String regex = "[,\n]";
         if (numbers.isEmpty()) return 0;
+
+        String delimiter = "[,|\n]";
+        String numbersWithoutDelimiter = numbers;
+
+        if (numbers.startsWith("//")) {
+            delimiter = String.valueOf(numbers.charAt(2));
+            numbersWithoutDelimiter = numbers.substring(numbers.indexOf("\n") + 1);
+        }
+
+        return sumNumbers(numbersWithoutDelimiter, delimiter);
+    }
+
+    private int sumNumbers(String numbers, String delimiter) {
         int result = 0;
 
-        String[] addedNumbers = numbers.split(regex);
+        String[] numberArray = numbers.split(delimiter);
 
-        for (String numberToAdd : addedNumbers) {
-            result += Integer.parseInt(numberToAdd);
+        for (String number : numberArray) {
+            result += Integer.parseInt(number);
         }
 
         return result;
