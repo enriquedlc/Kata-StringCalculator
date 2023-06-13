45ecbfc62c9b6b2c989ebc1efcc6745aee7458e4
Enrique <enriquedlc2d@gmail.com>
2023-06-13 23:03:38 +0100

    :heavy_check_mark: test should_return_add_of_two_numbers passed.

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,9 +1,14 @@
-import static java.lang.Integer.parseInt;
-
 public class StringCalculator {
     public int add(String numbers) {
         if (numbers.isEmpty()) return 0;
+        int result = 0;
 
-        return parseInt(numbers);
+        String[] addedNumbers = numbers.split(",");
+
+        for (String numberToAdd : addedNumbers) {
+            result += Integer.parseInt(numberToAdd);
+        }
+
+        return result;
     }
 }
