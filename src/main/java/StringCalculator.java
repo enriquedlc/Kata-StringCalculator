ebfd8c482e63c89611bbe36c48e2d3cc80bb559a
Enrique <enriquedlc2d@gmail.com>
2023-06-13 23:19:02 +0100

    :rotating_light: test should_add_numbers_with_newline_regex passed.

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,9 +1,10 @@
 public class StringCalculator {
     public int add(String numbers) {
+        String regex = "[,\n]";
         if (numbers.isEmpty()) return 0;
         int result = 0;
 
-        String[] addedNumbers = numbers.split(",");
+        String[] addedNumbers = numbers.split(regex);
 
         for (String numberToAdd : addedNumbers) {
             result += Integer.parseInt(numberToAdd);
