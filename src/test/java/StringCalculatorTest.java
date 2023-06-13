8644a581bb8cac2f65004b116dd4c0eba71e8666
Enrique <enriquedlc2d@gmail.com>
2023-06-13 23:41:13 +0100

    :rotating_light: test admitting_delimiter failed.

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -9,6 +9,13 @@ public class StringCalculatorTest {
      * EXAMPLE 1: "" => 0
      * EXAMPLE 2: "1" => 1
      * EXAMPLE 3: "1,2" => 3
+     * EXAMPLE 4: "1,2,3 => 6
+     * EXAMPLE 5: “1\n2,3” => 6
+     * EXAMPLE 6:
+     * “//[delimiter]\n[numbers…]”
+     * “//;\n1;2” => 3
+     * :rotating_light:
+     * :heavy_check_mark:
      */
 
     private final StringCalculator stringCalculator = new StringCalculator();
@@ -41,6 +48,12 @@ public class StringCalculatorTest {
     public void should_add_numbers_with_newline_regex() {
         int result = stringCalculator.add("1\n2,3");
         assertEquals(6, result);
+    }
+
+    @Test
+    public void admitting_delimiter() {
+        int result = stringCalculator.add("//;\n1;2");
+        assertEquals(3, result);
     }
 
 }
