95547addf5d4071ea8bc09b581dccf0648b7fa24
Enrique <enriquedlc2d@gmail.com>
2023-06-14 00:20:13 +0100

    Refactor:hammer:: added new methods to improve code readability.

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,16 +1,30 @@
 public class StringCalculator {
+
     public int add(String numbers) {
         if (numbers.isEmpty()) return 0;
 
-        String delimiter = "[,|\n]";
-        String numbersWithoutDelimiter = numbers;
-
-        if (numbers.startsWith("//")) {
-            delimiter = String.valueOf(numbers.charAt(2));
-            numbersWithoutDelimiter = numbers.substring(numbers.indexOf("\n") + 1);
-        }
+        String delimiter = getDelimiter(numbers);
+        String numbersWithoutDelimiter = getNumbersWithoutDelimiter(numbers);
 
         return sumNumbers(numbersWithoutDelimiter, delimiter);
+    }
+
+    private boolean startWithDoubleSlash(String numbers) {
+        return numbers.startsWith("//");
+    }
+
+    private String getNumbersWithoutDelimiter(String numbers) {
+        if (startWithDoubleSlash(numbers)) {
+            return numbers.substring(numbers.indexOf("\n") + 1);
+        }
+        return numbers;
+    }
+
+    private String getDelimiter(String numbers) {
+        if (startWithDoubleSlash(numbers)) {
+            return String.valueOf(numbers.charAt(2));
+        }
+        return "[,|\n]";
     }
 
     private int sumNumbers(String numbers, String delimiter) {
