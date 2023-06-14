8eca1fcec5717f3e0af248382161e3e3208ad109
Enrique <enriquedlc2d@gmail.com>
2023-06-14 01:31:10 +0100

    :hammer::hammer:

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,3 +1,4 @@
+import exception.NegativeNumberException;
 import org.junit.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,38 +10,56 @@ public class StringCalculatorTest {
      * EXAMPLE 1: "" => 0
      * EXAMPLE 2: "1" => 1
      * EXAMPLE 3: "1,2" => 3
+     * EXAMPLE 4: "1,2,3 => 6
+     * EXAMPLE 5: “1\n2,3” => 6
+     * EXAMPLE 6:
+     * “//[delimiter]\n[numbers…]”
+     * “//;\n1;2” => 3
+     * :rotating_light:
+     * :heavy_check_mark:
      */
 
-    private final StringCalculator stringCalculator = new StringCalculator();
+    private final StringCalculatorMain stringCalculator = new StringCalculatorMain();
 
     @Test
-    public void should_return_zero_if_empty_string_is_passed() {
+    public void should_return_zero_if_empty_string_is_passed() throws NegativeNumberException {
         int result = stringCalculator.add("");
         assertEquals(0, result);
     }
 
     @Test
-    public void should_return_single_parameter_number() {
+    public void should_return_single_parameter_number() throws NegativeNumberException {
         int result = stringCalculator.add("1");
         assertEquals(1, result);
     }
 
     @Test
-    public void should_return_addition_of_two_numbers() {
+    public void should_return_addition_of_two_numbers() throws NegativeNumberException {
         int result = stringCalculator.add("1,2");
         assertEquals(3, result);
     }
 
     @Test
-    public void should_return_addition_of_numbers_given() {
+    public void should_return_addition_of_numbers_given() throws NegativeNumberException {
         int result = stringCalculator.add("1,2,3");
         assertEquals(6, result);
     }
 
     @Test
-    public void should_add_numbers_with_newline_regex() {
+    public void should_add_numbers_with_newline_regex() throws NegativeNumberException {
         int result = stringCalculator.add("1\n2,3");
         assertEquals(6, result);
+    }
+
+    @Test
+    public void admitting_delimiter() throws NegativeNumberException {
+        int result = stringCalculator.add("//;\n1;2");
+        assertEquals(3, result);
+    }
+
+    @Test
+    public void should_throw_exception_if_there_is_a_negative_number() {
+        assertThrows(NegativeNumberException.class, () -> stringCalculator.add("//;\n1;2;-5"));
     }
 
 }
