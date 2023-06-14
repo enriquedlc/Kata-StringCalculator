9a84565c19e56dcb98268815535a40de08f59711
Enrique <enriquedlc2d@gmail.com>
2023-06-14 01:07:35 +0100

    Refactor:hammer:: moved helper methods for the main function.
    moved helper methods into src/java/utils/ to more readable code.

---
+++ src/main/java/StringCalculatorMain.java
@@ -0,0 +1,16 @@
+import exception.NegativeNumberException;
+import utils.StringCalculator;
+
+public class StringCalculatorMain {
+
+    StringCalculator utils = new StringCalculator();
+
+    public int add(String numbers) throws NegativeNumberException {
+        if (numbers.isEmpty()) return 0;
+
+        String delimiter = utils.getDelimiter(numbers);
+        String numbersWithoutDelimiter = utils.getNumbersWithoutDelimiter(numbers);
+
+        return utils.sumNumbers(numbersWithoutDelimiter, delimiter);
+    }
+}
--- src/main/java/utils/StringCalculator.java
+++ src/main/java/utils/StringCalculator.java
@@ -1,40 +1,32 @@
+package utils;
+
 import exception.NegativeNumberException;
 
-import java.util.Arrays;
-
 public class StringCalculator {
-    public int add(String numbers) throws NegativeNumberException {
-        if (numbers.isEmpty()) return 0;
 
-        String delimiter = getDelimiter(numbers);
-        String numbersWithoutDelimiter = getNumbersWithoutDelimiter(numbers);
-
-        return sumNumbers(numbersWithoutDelimiter, delimiter);
-    }
-
-    private boolean isNegative(String number) {
+    public boolean isNegative(String number) {
         return number.startsWith("-");
     }
 
-    private boolean startWithDoubleSlash(String numbers) {
+    public boolean startWithDoubleSlash(String numbers) {
         return numbers.startsWith("//");
     }
 
-    private String getNumbersWithoutDelimiter(String numbers) {
+    public String getNumbersWithoutDelimiter(String numbers) {
         if (startWithDoubleSlash(numbers)) {
             return numbers.substring(numbers.indexOf("\n") + 1);
         }
         return numbers;
     }
 
-    private String getDelimiter(String numbers) {
+    public String getDelimiter(String numbers) {
         if (startWithDoubleSlash(numbers)) {
             return String.valueOf(numbers.charAt(2));
         }
         return "[,|\n]";
     }
 
-    private int sumNumbers(String numbers, String delimiter) throws NegativeNumberException {
+    public int sumNumbers(String numbers, String delimiter) throws NegativeNumberException {
         int result = 0;
 
         String[] numberArray = numbers.split(delimiter);
@@ -48,4 +40,5 @@ public class StringCalculator {
 
         return result;
     }
+
 }
